6507402645d39f9f53a1417bee7fe3b7afb30d08
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-01-08 23:58:11 +0000

    pictures

--- src/components/FlashCards/index.jsx
+++ src/components/FlashCards/index.jsx
@@ -2,36 +2,17 @@ import dataForAvengers from "../DataForAvengers";
 import styles from "./index.module.scss";
 
 const FlashCard = () => {
-	return <div className={styles.hero}>hello</div>;
+	return (
+		<div className={styles.hero}>
+			{dataForAvengers.map((card) => (
+				<div key={card.id} className={styles.flashCard}>
+					<img src={card.image} alt="" />
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default FlashCard;
 
-////////////////////
-// const UserCard = () => {
-//   // const [isShow, setIsToogle] = (useState = false);
-//   return (
-//     <div className={styles.hero}>
-//       {dataForUsers.map((card) => (
-//         <div key={card.id} className={styles.flipCard}>
-//           <div className={styles.flipCardInner}>
-//             <div className={styles.flipCardFront}>
-//               <Image
-//                 className={styles.picture}
-//                 key={card?.id}
-//                 src={card.image}
-//                 alt='card'
-//                 width={450}
-//                 height={500}
-//                 quality={80}
-//               />
-//             </div>
-//             <div className={styles.flipCardBack}>
-//               <UserData card={card} />
-//             </div>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+
