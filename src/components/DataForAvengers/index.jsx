52d2b2c78314feeea2dd4bf2d97294eaeb52ca1b
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-01-08 22:23:55 +0000

    layout

---
+++ src/components/DataForAvengers/index.jsx
@@ -0,0 +1,48 @@
+import pictureOne from "../images/Batman.jpg";
+import pictureTwo from "../images/BlackWidow.jpg";
+import pictureThree from "../images/Hulk.jpg";
+import pictureFour from "../images/Ironman.jpg";
+import pictureFive from "../images/Spiderman.jpg";
+import pictureSix from "../images/Thor.jpg";
+
+const dataForAvengers = [
+	{
+		id: 1,
+		image: pictureOne,
+		paragraph:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+	},
+	{
+		id: 2,
+		image: pictureTwo,
+		paragraph:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+	},
+	{
+		id: 3,
+		image: pictureThree,
+		paragraph:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+	},
+	{
+		id: 4,
+		image: pictureFour,
+		paragraph:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+	},
+	{
+		id: 5,
+		image: pictureFive,
+		paragraph:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+	},
+	{
+		id: 6,
+		image: pictureSix,
+		paragraph:
+			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+	},
+];
+console.log(dataForAvengers);
+
+export default dataForAvengers;
