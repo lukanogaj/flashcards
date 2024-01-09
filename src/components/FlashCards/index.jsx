2de3c87bbab8afc14e439ec7393f55e2f19419c7
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-01-09 13:51:15 +0000

    handleClick

--- src/components/FlashCards/index.jsx
+++ src/components/FlashCards/index.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import dataForAvengers from "../DataForAvengers";
 import styles from "./index.module.scss";
 
 const FlashCard = () => {
+	const [selectedId, setSelectedId] = useState("");
+
+	const handleCLick = () => {
+		setSelectedId(!selectedId);
+	};
+	// console.log(handleCLick());
 	return (
 		<div className={styles.hero}>
 			{dataForAvengers.map((card) => (
-				<div key={card.id} className={styles.flashCard}>
+				<div key={card.id} className={styles.flashCard} onClick={handleCLick}>
 					<img src={card.image} alt="" />
+					<p>Hello</p>
 				</div>
 			))}
+			console.log(handleCLick());
 		</div>
 	);
 };
 
 export default FlashCard;
-
-
